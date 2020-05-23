2ad512037b4501288fed2aaddb84d740a8512204
Utkarsh Chakravarty <chakravartyutkarsh@gmail.com>
2020-05-23 21:39:19 +0530

    Score calculated and reset quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/quiz.dart';
 
-import './questions.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,44 +14,51 @@ class MyApp extends StatefulWidget {
   }
 }
 
-
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  var _question = [
+    {
+      'questionText': 'How are you?',
+      'answer': [{'text': 'Fine', 'score': 30}, {'text': 'Good', 'score': 20}, {'text': 'Bad', 'score': 10}, {'text': 'Awesome', 'score': 100}]
+    },
+    {
+      'questionText': 'Where are you?',
+      'answer': [{'text': 'Home', 'score': 30}, {'text': 'College', 'score': 20}, {'text': 'Gym', 'score': 10}]
+    },
+  ];
+
+  void _resetQuiz() {
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      _totalScore = 0;
+      _questionIndex = 0;
     });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore +=score;
+
+    if (_questionIndex < _question.length) {
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+    }
     print(_questionIndex);
+    print(_totalScore);
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      {
-        'questionText': 'How are you?',
-        'answer': [ 'Fine', 'Good', 'Bad', 'Awesome']
-      },
-      {
-        'questionText': 'Where are you?',
-        'answer': [ 'Home', 'College', 'Gym']
-      },
-    ];
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _question.length
+            ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _question,)
+            : Result(resultScore: _totalScore, resetHandler: _resetQuiz),
       ),
-      body: Column(
-        children: <Widget>[
-          Question(question[_questionIndex]['questionText']
-      ),
-          ...(question[_questionIndex]['answer'] as List<String>)
-          .map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList()
-        ],
-      ),
-    ),
     );
   }
 }
---
+++ lib/result.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result({this.resultScore, this.resetHandler});
+
+  String get total {
+    String message;
+    if (resultScore > 40) {
+      message = 'You are awesome! ;)';
+    } else {
+      message = 'You need to work hard :(';
+    }
+    return message;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: <Widget>[
+        Text(
+          total,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        FlatButton(
+          child: Text('Restart quiz',
+            style: TextStyle(fontSize: 24
+            ),
+          ),
+          textColor: Colors.blue,
+          onPressed: resetHandler,
+        ),
+      ],
+    ));
+  }
+}
